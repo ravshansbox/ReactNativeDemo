7641fb588b6b9f9ce6fea444a0bbaddbb5ee2243
Ravshan Samandarov <ravshansbox@gmail.com>
2025-06-05 14:17:35 +0300

    Initial commit
    Generated by react-native@0.79.3

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <View>
+      <Text>Example text</Text>
+    </View>
+  );
+}
+export default App;
