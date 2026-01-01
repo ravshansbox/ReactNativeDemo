e504e70e8c2c5be0e210bb58bba4ff6c2806b122
Ravshan Samandarov <ravshansbox@gmail.com>
2026-01-02 02:21:03 +0300

    enable picture-in-picture

--- App.tsx
+++ App.tsx
@@ -1,28 +1,47 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { useRef } from 'react';
+import { Button, StyleSheet, View } from 'react-native';
+import Video, { VideoRef } from 'react-native-video';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+const uris = [
+  'https://www.w3schools.com/html/mov_bbb.mp4',
+  'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+];
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  const currentUriIndex = useRef(0);
+  const videoRef = useRef<VideoRef>(null);
+  const isInPipRef = useRef(false);
 
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      <Video
+        ref={videoRef}
+        style={styles.video}
+        source={{ uri: uris[currentUriIndex.current] }}
+        controls
+        enterPictureInPictureOnLeave
+        onPictureInPictureStatusChanged={({ isActive }) => {
+          isInPipRef.current = isActive;
+        }}
+        onEnd={() => {
+          if (currentUriIndex.current < uris.length - 1) {
+            currentUriIndex.current++;
+          }
+          videoRef.current?.setSource({ uri: uris[currentUriIndex.current] });
+          videoRef.current?.resume();
+        }}
+      />
+      <Button
+        title="PiP"
+        onPress={() => videoRef.current?.enterPictureInPicture()}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+  container: { flex: 1 },
+  video: { width: '100%', aspectRatio: 16 / 9 },
 });
 
 export default App;
